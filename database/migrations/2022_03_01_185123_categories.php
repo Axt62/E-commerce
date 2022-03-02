b0597edefdcb8596627275634bf02f706159d171
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-02 07:54:51 +0100

    Ajout de la table catégories

--- database/migrations/2022_03_01_185123_categories.php
+++ database/migrations/2022_03_01_185123_categories.php
@@ -4,9 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
 return new class extends Migration
-
 {
     /**
      * Run the migrations.
@@ -15,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('produits', function (Blueprint $table) {
-            $table->id();
-            $table->string('nom');
-            $table->string('description');
-            $table->decimal('prix');
+        Schema::create('categories', function (Blueprint $table) {
+
             $table->string('slug');
-            $table->timestamp('created_at');
-            $table->string('img');
-            $table->decimal('promotion');
+            $table->string('nom');
     });
 }
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\accueilcontroller;
+use App\Http\Controllers\categoriescontroller;
 use App\Http\Controllers\contactcontroller;
 use App\Http\Controllers\produitscontroller; // Page produitS   
 use App\Http\Controllers\paniercontroller;
@@ -22,3 +23,4 @@ Route::get('/produits', [produitscontroller::class, 'Index']);
 Route::get('/produit/{produits}', [produitscontroller::class, 'show']);
 Route::get('/contact', [contactcontroller::class, 'Index']);
 Route::get('/panier', [paniercontroller::class, 'Index']);
+Route::get('/categorie', [categoriescontroller::class, 'index']);

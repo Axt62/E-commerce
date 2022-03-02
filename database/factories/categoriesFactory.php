716e5f3ae80e1aeffc5d93a91a05aae20c8de624
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-02 10:43:57 +0100

    Ajout seeder et factory

---
+++ database/factories/categoriesFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\produits>
+ */
+class produitsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'slug' => $this->faker->slug(),
+            'nom' => $this->faker->nom(),
+        ];
+        
+    }
+}
